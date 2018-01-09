69d17e3409186359716821253a88330324ad67bb
Hugo Rune <31449959+rawnney@users.noreply.github.com>
2018-01-09 13:58:18 +0100

    fix directory

--- pear/src/App.js
+++ pear/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
+import Welcome from './Components/Welcome'
 
 class App extends Component {
   render () {
     return (
       <div className="App">
+        <Welcome />
       </div>
     )
   }
