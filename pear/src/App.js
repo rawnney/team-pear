bf34f9f8c343c1877403b3895d76a0c2f368df02
masudulhas <masudul.has@gmail.com>
2018-01-17 15:23:30 +0100

    unstyle info page

--- pear/src/App.js
+++ pear/src/App.js
@@ -1,23 +1,32 @@
 import React, { Component } from 'react'
-import Welcome from './Components/Welcome'
-import { Button } from 'reactstrap'
-import Nav from './Components/Nav'
-import './assets/css/main.css'
+import { Route, NavLink, HashRouter } from 'react-router-dom'
+import Home from './Home'
+import Stuff from './Stuff'
+import Contact from './Contact'
+import './index.css'
+// import Welcome from './Components/Welcome'
+// import { Button } from 'reactstrap'
+// import Nav from './Components/Nav'
+// import './assets/css/main.css'
 
 class App extends Component {
   render () {
     return (
-      <div>
-        <h1>Simple SPA</h1>
-        <ul className="header">
-          <li><a href="/">Home</a></li>
-          <li><a href="/stuff">Stuff</a></li>
-          <li><a href="/contact">Contact</a></li>
-        </ul>
-        <div className="content">
-
+      <HashRouter>
+        <div>
+          <h1>Game Info</h1>
+          <ul className="header">
+            <li><NavLink to="/">Home</NavLink></li>
+            <li><NavLink to="/stuff">Stuff</NavLink></li>
+            <li><NavLink to="/contact">Contact</NavLink></li>
+          </ul>
+          <div className="content">
+            <Route exact path="/" component={Home}/>
+            <Route path="/stuff" component={Stuff}/>
+            <Route path="/contact" component={Contact}/>
+          </div>
         </div>
-      </div>
+      </HashRouter>
     )
   }
 }
--- pear/src/index.js
+++ pear/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 import App from './App'
+import './index.css'
 import registerServiceWorker from './registerServiceWorker'
-import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap/dist/css/bootstrap.css'
 
-ReactDOM.render(<div> <App /> </div>, document.getElementById('root'))
+ReactDOM.render(<div> <App /> </div>,
+  document.getElementById('root'))
 registerServiceWorker()
