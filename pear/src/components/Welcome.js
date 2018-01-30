07acf9402ec070042db62a16c398446628d45c97
Nasimjibs <nasimjibs@gmail.com>
2018-01-30 12:33:42 +0100

    fixed login/logout

---
+++ pear/src/components/Welcome.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+const Welcome = ({user, onSignOut}) => {
+  // This is a dumb "stateless" component
+  return (
+    <div>
+      Welcome <strong>{user.username}</strong>!
+      <button type="button" className="btn btn-danger ribbon" onClick={onSignOut} >Sign Out</button>
+
+    </div>
+  )
+}
+
+export default Welcome
