6ba8d8d245c6bef977d494869333c0547ef46764
Robinwiss <robin@wisseng.se>
2018-01-30 11:21:13 +0100

    refixed bootstrap modals

--- pear/src/libs/Imgs.js
+++ pear/src/libs/Imgs.js
@@ -1,5 +1,10 @@
 export default {
   Monster: require('../assets/img/icons/monster-icon.png'),
-  Robin: require('../assets/img/icons/robin.png')
+  Robin: require('../assets/img/icons/robin.png'),
+  Sword: require('../assets/img/sword.png'),
+  Wand: require('../assets/img/wand.png'),
+  Shield: require('../assets/img/shield.png'),
+  Dagger: require('../assets/img/dagger.png'),
+  Armor: require('../assets/img/armor.png')
 }
 //  Working
