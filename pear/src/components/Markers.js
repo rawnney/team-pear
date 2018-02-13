699fbb9f1aaa4127f8cdfc0ef8e83a2e44233361
Tim Aro <timaro@live.se>
2018-02-13 15:27:42 +0100

    map skin added

--- pear/src/components/Markers.js
+++ pear/src/components/Markers.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps'
 import FightView from './FightView'
-import skin from '../assets/json/skin.json'
+import mapOptions from '../assets/json/skin'
 
 const MARKERS = withScriptjs(withGoogleMap(props => {
   let {lng, lat, markers} = props
-  return <GoogleMap defaultZoom={18} defaultOptions={{scrollwheel: false}} style={{skin}} defaultCenter={{lat, lng}}>
+
+  return <GoogleMap defaultZoom={18} defaultOptions={{scrollwheel: false}} options={mapOptions} /*styles={{skin}}*/ defaultCenter={{lat, lng}}>
     {<Marker position={{lat, lng}} />}
     {markers.map((marker, index) => (
       <Marker key={index} position={{lat: marker.latitude, lng: marker.longitude}} icon={marker.icon} clickable onClick={() => onClick(marker.id, props)} />
