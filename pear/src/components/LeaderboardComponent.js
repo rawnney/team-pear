ed149706026d50c046d5bf8bbd2323562205d621
Robinwiss <robin@wisseng.se>
2018-02-21 15:18:18 +0100

    Added leaderboard with that sort users on kills

--- pear/src/components/LeaderboardComponent.js
+++ pear/src/components/LeaderboardComponent.js
@@ -1,55 +1,59 @@
 import React, {Component} from 'react'
 // eslint-disable-next-line
 import {Table} from 'reactstrap'
+import axios from 'axios'
+const API_GETUSERS = `https://peargameapi.herokuapp.com/api/users`
 
 export default class Leaderboard extends Component {
   constructor (props) {
     super(props)
-    this.state = {}
+    this.state = {
+      leaderboardData: []
+    }
   }
-
   componentDidMount () {
-    // getAllUsers = () => {}
+    axios.get(API_GETUSERS)
+      .then(res => {
+        const leaderboardData = res.data.Users
+        this.setState({ leaderboardData })
+        console.log(JSON.stringify(leaderboardData))
+      })
   }
 
   render () {
+    function sortData (first, second) {
+      let firstTime = first.monstersKilled
+      let secondTime = second.monstersKilled
+      if (firstTime === secondTime) { return 0 }
+      if (firstTime > secondTime) { return -1 } else { return 1 }
+    }
+    let list = this.state.leaderboardData.sort(sortData).map((leaderboardData) => {
+      return (
+        <tr>
+          <th><li></li></th>
+          <td>{leaderboardData.username}</td>
+          <td>{leaderboardData.team}</td>
+          <td>{leaderboardData.monstersKilled}</td>
+
+        </tr>
+      )
+    })
     return <div>
-      <Table>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Username</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td>Larry</td>
-            <td>the Bird</td>
-            <td>@twitter</td>
-          </tr>
-        </tbody>
-      </Table>
+      <ol>
+        <Table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Username</th>
+              <th>Team</th>
+              <th>Monsters Killed</th>
+            </tr>
+          </thead>
+          <tbody>
+            {list}
+          </tbody>
+        </Table>
+      </ol>
     </div>
   }
-
-  getAllUsers = () => {}
-
-  sortUsers = () => {}
-
-  returnSortedUsers = () => {}
 }
