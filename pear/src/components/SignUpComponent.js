5cae38a974792a63adbeb5a8bb17e0f2753826a7
Nasimjibs <nasimjibs@gmail.com>
2018-03-01 12:02:59 +0100

    small change

--- pear/src/components/SignUpComponent.js
+++ pear/src/components/SignUpComponent.js
@@ -3,44 +3,33 @@ import { AvForm, AvField, AvGroup, AvInput, AvFeedback, AvRadioGroup, AvRadio } 
 import { Button, Label, FormGroup } from 'reactstrap'
 import Images from '../libs/Imgs'
 import {capitalizeFirstLetter} from '../libs/Common'
-import axios from 'axios'
 
 let {Avatar1, Avatar2, Avatar3, Avatar4 } = Images
-const API_USERS = 'http://peargameapi.herokuapp.com/api/users/'
 
 export default class Example extends React.Component {
   constructor (props) {
     super(props)
-    this.handleSubmit = this.handleSubmit.bind(this)
+
+    this.handleInvalidSubmit = this.handleInvalidSubmit.bind(this)
     this.state = {user: {}}
   }
 
-  handleSubmit (event, errors, values) {
-    if (values.originalEmail === '' || values.originalEmail === '') {
-      errors.push('originalEmail', 'confirmationEmail')
-    }
+  handleInvalidSubmit (event, errors, values) {
     this.setState({errors, values})
-
-    // const { username, originalEmail, password, avatar, selectTeam } = this.state.values
-    this.setState({user: this.state.values})
-    // axios.post(API_USERS, { username, password, originalEmail, avatar, selectTeam })
-    //   .then((result) => {
-    //     const user = null
-    //     if (onSignIn) onSignIn(user)
-    //   })
+  }
+  handleUsername = (username) => {
+    let {user} = this.state
+    this.setState({user: {...user, username: capitalizeFirstLetter(username.target.value)}})
   }
 
   render () {
     return (
       <div>
-        <AvForm onSubmit={this.handleSubmit}>
-          {this.state.values && <div style={styles.error}>
-            <div>Invalid Submission: {this.state.errors.join(', ')}<br /></div>
+        <AvForm onInvalidSubmit={this.handleInvalidSubmit}>
 
-          </div>}
           <AvGroup>
             <Label for="username" sm={4}>Username</Label>
-            <AvInput type="username" name="username" id="username" placeholder="Ex: 'BootyWarrior'" required />
+            <AvInput type="username" name="username" onChange={this.handleUsername} id="username" placeholder="Ex: 'BootyWarrior'" required />
             {/* this only shows when there is an error, use reactstrap's FormFeedback if you want is to always be displayed */}
             <AvFeedback>This is an error!</AvFeedback>
           </AvGroup>
@@ -55,13 +44,8 @@ export default class Example extends React.Component {
             <AvInput name="confirmationEmail" id="confirmationEmail" type="email" placeholder="example.email@example.com" validate={{match: {value: 'originalEmail'}}}/>
           </AvGroup>
 
-          <AvGroup>
-            <Label for="password" sm={4}>Password</Label>
-            <AvInput name="password" id="password" type="password" required/>
-          </AvGroup>
-
           {/* Radios */}
-          <AvRadioGroup name="avatar" label="Choose Your Avatar" required>
+          <AvRadioGroup name="avatar" label="Choose Avatar" required>
             <Label check>
               <AvRadio value="Avatar1" id="Avatar1" />
               <img style={styles.avatar} src={Avatar1} alt='Avatar1'/>
@@ -82,9 +66,8 @@ export default class Example extends React.Component {
           </AvRadioGroup>
           {/* With select and AvField */}
 
-          <AvField type="select" name="selectTeam" label="Select a Team" helpMessage="Select a team" required>
-            <option value='' >Select a team</option>
-            <option value='1' >Red</option>
+          <AvField type="select" name="selectTeam" label="Select a Team" helpMessage="Select a team">
+            <option value='1'>Red</option>
             <option value='2'>Green</option>
             <option value='3'>Blue</option>
           </AvField>
@@ -92,7 +75,11 @@ export default class Example extends React.Component {
             <Button>Submit</Button>
           </FormGroup>
         </AvForm>
-
+        {this.state.values && <div>
+          <h5>Submission values</h5>
+          Invalid: {this.state.errors.join(', ')}<br />
+          Values: <pre>{JSON.stringify(this.state.values, null, 2)}</pre>
+        </div>}
       </div>
     )
   }
@@ -101,8 +88,5 @@ export default class Example extends React.Component {
 let styles = {
   avatar: {
     height: '150px'
-  },
-  error: {
-    color: '#c52e43'
   }
 }
